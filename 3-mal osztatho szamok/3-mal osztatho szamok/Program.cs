404c74b863a68011841a82cd7b03438bc57d8bef
Rbencuska69 <sutkabence2006@gmail.com>
2024-09-17 11:30:17 +0200

    alap

---
+++ 3-mal osztatho szamok/3-mal osztatho szamok/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_mal_osztatho_szamok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg húszig egy számot:");
+            int szam = Convert.ToInt32(Console.ReadLine()) ;
+            switch (szam)
+            {
+                case 3:
+                    Console.WriteLine("3-mal osztható számok");
+                    break;             
+                case 6:                
+                    Console.WriteLine("3-mal osztható számok");
+                    break;            
+                case 9:               
+                    Console.WriteLine("3-mal osztható számok");
+                    break;            
+                case 12:             
+                    Console.WriteLine("3-mal osztható számok");
+                    break;            
+                case 15:              
+                    Console.WriteLine("3-mal osztható számok");
+                    break;
+                case 18:
+                    Console.WriteLine("3-mal osztható számok");
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Nem osztható 3-mal");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
